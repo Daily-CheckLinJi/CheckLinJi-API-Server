a79d7fd2c5eacbf43f816caed1dbf228e0bac822
손종국 <kyky7852@naver.com>
2023-11-15 20:40:34 +0900

    코드 수정

--- CheckLinJi-API/src/main/java/com/upside/api/controller/ChallengeController.java
+++ CheckLinJi-API/src/main/java/com/upside/api/controller/ChallengeController.java
@@ -103,7 +103,7 @@ public class ChallengeController {
 	@PostMapping("/userDetail") 
 	public ResponseEntity<Map<String, Object>> userList (@RequestHeader("Authorization") String authHeader , @RequestBody PageDto pageDto) {
 				
-		Map<String, Object> result = challengeSerivce.viewChallengeList(pageDto);
+		Map<String, Object> result = challengeSerivce.userChallengeList(pageDto);
 		
 		if (result.get("HttpStatus").equals("2.00")) { // 성공			
 			return new ResponseEntity<>(result,HttpStatus.OK);					
